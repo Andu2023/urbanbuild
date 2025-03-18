c91851e8d87aeedf2fd6c99d88824fdb1ff2bc86
Andu2023 <andy2023user@gmail.com>
2025-03-18 08:22:03 -0700

    link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,25 @@
-// import { Link as ScrollLink } from "react-scroll"
+"use client";
+import { Link as ScrollLink } from "react-scroll"
+const Links= [
+  {
+  name:"home",
+  path:"home",
+},
+{
+  name:"home",
+  path:"home",
+},
+{
+  name:"home",
+  path:"home",
+},
+{
+  name:"home",
+  path:"home",
+},
+
+
+]
 const Header = () => {
   return (
     <div>header </div>
